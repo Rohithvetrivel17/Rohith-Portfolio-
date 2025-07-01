965dc0ef8dbeecb107c520814130aac2e9760e48
Rohithvetrivel17 <rohithkumar1702@gmail.com>
2025-07-01 17:04:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-const acc = document.getElementsByClassName("accordion");
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    const panel = this.nextElementSibling;
-    panel.style.display = panel.style.display === "block" ? "none" : "block";
-  });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".accordion-button");
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", function () {
+            const content = this.nextElementSibling;
+            content.style.display = content.style.display === "block" ? "none" : "block";
+        });
+    });
+});
